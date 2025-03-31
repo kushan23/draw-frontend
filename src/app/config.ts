be88e7e89e7dbd89ab0be9ee69362f0b927ea3e6
Kushan Arora <kushan@192.168.1.4>
2025-03-31 19:23:04 +0530

    fixed config

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,9 +1,4 @@
-export const BACKEND_URL = 
-  typeof window !== "undefined" && window.NEXT_RUNTIME_ENV?.BACKEND_URL 
-    ? window.NEXT_RUNTIME_ENV.BACKEND_URL 
-    : process.env.NEXT_PUBLIC_BACKEND_URL;
+export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
+export const WS_URL = process.env.NEXT_PUBLIC_WS_URL;
 
-export const WS_URL = 
-  typeof window !== "undefined" && window.NEXT_RUNTIME_ENV?.WS_URL 
-    ? window.NEXT_RUNTIME_ENV.WS_URL 
-    : process.env.NEXT_PUBLIC_WS_URL;
+
