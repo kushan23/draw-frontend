4cb228ae0b1ade54ea207271ca51bad4e9313d00
Kushan Arora <kushan@192.168.1.2>
2025-04-02 22:05:43 +0530

    Url fix

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,2 +1,5 @@
-export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL
-export const WS_URL = process.env.NEXT_PUBLIC_WS_URL
+// export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL
+// export const WS_URL = process.env.NEXT_PUBLIC_WS_URL
+
+export const BACKEND_URL= "http://draw.kushan23.com/api"
+export const WS_URL=  "ws://draw.kushan23.com/ws"
