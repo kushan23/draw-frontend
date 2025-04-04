e5c2e7814a130a6647168f0061c1d297a944d6f6
Kushan Arora <kushan@192.168.1.6>
2025-04-04 14:16:14 +0530

    url fiz

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,5 +1,5 @@
 // export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL
 // export const WS_URL = process.env.NEXT_PUBLIC_WS_URL
 
-export const BACKEND_URL= "http://draw.kushan23.com/api"
-export const WS_URL=  "ws://draw.kushan23.com/ws"
+export const BACKEND_URL= "https://draw.kushan23.com/api"
+export const WS_URL=  "wss://draw.kushan23.com/ws"
